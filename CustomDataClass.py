b797b87f991a1f03deb2a5be39f38501a507f8c9
jpe9at <151448946+jpe9at@users.noreply.github.com>
2024-04-07 11:56:34 +0200

    Update CustomDataClass.py

--- CustomDataClass.py
+++ CustomDataClass.py
@@ -6,8 +6,10 @@ import pandas as pd
 
 class Data(Dataset):
     def __init__(self, X, y):
+        #needed to transform input tensor to float.
         X = np.array([arr.astype(float) for arr in X.values], dtype=float)
         self.X = torch.tensor(X, dtype = torch.float32)
+        #prediction
         self.y = torch.tensor(y, dtype = torch.long)
         self.len = self.X.shape[0]
        
@@ -17,12 +19,7 @@ class Data(Dataset):
     def __len__(self):
         return self.len
 
-'''
-#How to deal with other types of data? 
-'''
-
 class DataModule: 
-    """The base class of data."""
     def __init__(self, X,y):
         self.dataset = Data(X,y)
 
@@ -33,25 +30,8 @@ class DataModule:
         return self.get_dataloader(batch_size)
 
     def val_dataloader(self, batch_size):
-        #here say something about gradients not required ? 
         return self.get_dataloader(batch_size)
 
-#data = self.train if train else self.val
-#    return torch.utils.data.DataLoader(data, self.batch_size, shuffle=train,num_workers=self.num_workers)
-
-#
-#dataframe = pd.read_csv('~/Datasets/Deli Climate/DailyDelhiClimateTrain.csv')
-#time_series_data = dataset_for_time_series(dataframe.iloc[:,1:],4) 
-#data = DataModule(time_series_data[0],time_series_data[1])
-
-
-#num of workers for allowing of multi processing
-    #num_workers=0 means that it’s the main process that does the data loading when needed.
-    #num_workers=1 means you only have a single worker, so it might be slow.
-
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-kwargs = {'num_workers': 1, 'pin_memory': True} if device=='cuda' else {}
 
 
 
