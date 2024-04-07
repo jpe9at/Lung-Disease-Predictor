364625338d7500a4486f8ed320b6220be6328692
jpe9at <151448946+jpe9at@users.noreply.github.com>
2024-04-07 12:06:57 +0200

    Update Lung_predictor.py

--- Lung_predictor.py
+++ Lung_predictor.py
@@ -1,4 +1,4 @@
-from CustomDataClass import DataModule, Data 
+from CustomDataClass import DataModule, Data, merge_dfs, create_dataframe_with_image_data 
 from Module import CNNLungs
 import Trainer 
 import pandas as pd
@@ -12,36 +12,43 @@ import os
 import cv2
 
 
+
+######################################
+#Load the Data and create Data Class
+######################################
+
+print('Load training data')
+
 home_directory = os.path.expanduser('~')
 
-#Training Data
 directory1 = home_directory + '/Datasets/chest_xray/train/NORMAL'
 directory2 = home_directory + '/Datasets/chest_xray/train/PNEUMONIA'
 
-print('load td')
 df_train_neg = create_dataframe_with_image_data(directory2)
 df_train_pos = create_dataframe_with_image_data(directory2)
 df_train = merge_dfs(df_train_neg, df_train_pos)
 
-#Validation Data
+print('Load validation data')
+
 directory1 = home_directory + '/Datasets/chest_xray/val/NORMAL'
 directory2 = home_directory + '/Datasets/chest_xray/val/PNEUMONIA'
 
-print('load val data')
 df_val_neg = create_dataframe_with_image_data(directory2)
 df_val_pos = create_dataframe_with_image_data(directory2)
 df_val = merge_dfs(df_val_neg, df_val_pos)
 
-#Testing Data
+print('Load test data')
+
 directory1 = home_directory + '/Datasets/chest_xray/test/NORMAL'
 directory2 = home_directory + '/Datasets/chest_xray/test/PNEUMONIA'
 
-print('load testd')
 df_test_neg = create_dataframe_with_image_data(directory2)
 df_test_pos = create_dataframe_with_image_data(directory2)
 df_test = merge_dfs(df_test_neg, df_test_pos)
 
-print('data Module')
+
+print('Creating Data Module')
+
 X_train =df_test.iloc[:,0]
 y_train =df_test.iloc[:,1]
 data_train = DataModule(X_train,y_train)
@@ -54,28 +61,32 @@ X_test =df_test.iloc[:,0]
 y_test =df_test.iloc[:,1]
 data_test = DataModule(X_test,y_test)
 
-print('init cnn')
-#cnn_model= CNNLungs(64, 3, learning_rate = 0.0001)
-                     #, best_params['optimizer'], best_params['learning_rate'],  param_initialisation = best_params['weight_initialisation']) 
-#trainer = Trainer.Trainer(50, 8, early_stopping_patience = 10)
 
-print('start fitting')
+######################################
+#Initialise and train CNN
+######################################
 
-'''
-trainer.fit(cnn_model,data_train,data_val)
-best_params, best_accuracy = Trainer.Trainer.hyperparameter_optimization(data_train, data_test)
+if hyperparameter_optimization == True: 
+  
+  trainer.fit(cnn_model,data_train,data_val)
+  best_params, best_accuracy = Trainer.Trainer.hyperparameter_optimization(data_train, data_test)
+  
+  cnn_model= CNNLungs(best_params['hidden_size'], 3, optimizer = best_params['optimizer'], learning_rate = best_params['learning_rate'], l2 = best_params  param_initialisation = best_params['weight_initialisation']) 
+  trainer = Trainer.Trainer(50, best_params['batch_size'], early_stopping_patience = 100)
+  trainer.fit(cnn_model,data_train,data_val)
 
-cnn_model= CNNLungs(best_params['hidden_size'], 3, optimizer = best_params['optimizer'], learning_rate = best_params['learning_rate'], l2 = best_params  param_initialisation = best_params['weight_initialisation']) 
-trainer = Trainer.Trainer(50, best_params['batch_size'], early_stopping_patience = 100)
-trainer.fit(cnn_model,data_train,data_val)
-'''
+else: 
+  cnn_model= CNNLungs(32, 3, optimizer = 'Adam', learning_rate = 0.00007, param_initialisation = (None,'He'), scheduler = 'OnPlateau', l1 = 0.0, clip_val = 1.5, l2 = 0.1 ) 
+  trainer = Trainer.Trainer(100, early_stopping_patience = 10)
+  trainer.fit(cnn_model,data_train,data_val)
 
-cnn_model= CNNLungs(32, 3, optimizer = 'Adam', learning_rate = 0.00007, param_initialisation = (None,'He'), scheduler = 'OnPlateau', l1 = 0.0, clip_val = 1.5, l2 = 0.1 ) 
-trainer = Trainer.Trainer(100, early_stopping_patience = 10)
-trainer.fit(cnn_model,data_train,data_val)
+
+######################################
+#Visualize Results
+######################################
+
 
 test_loss = trainer.test(cnn_model,data_test)
-
 print(f'Test Loss is {test_loss:.4f}')
 
 n_epochs = range(trainer.max_epochs)
@@ -87,12 +98,15 @@ val_loss = trainer.val_loss_values
 nan_values = np.full(trainer.max_epochs - len(val_loss), np.nan)
 val_loss = np.concatenate([val_loss,nan_values])
 
-
-
 plt.figure()
 plt.plot(n_epochs, train_loss, color='blue', label='train_loss' , linestyle='-')
 plt.plot(n_epochs, val_loss, color='orange', label='val_loss' , linestyle='-')
 plt.legend()
+
+
+######################################
+#Use the DeepSHAP explainer
+######################################
 
 examples = data_test.dataset.X[:-3]
 test = data_test.dataset.X[-3:]
@@ -110,37 +124,8 @@ fig, axs = plt.subplots(nrows=7, ncols=1, figsize=(8, 2*7))
 for i in range(7):
     shap.image_plot(shap_values[i], examples[i].numpy(), show=False, ax=axs[i])
 
+plt.figure()
 plt.tight_layout()
 plt.show()
 
 
-'''
-y_hat, y = trainer.test(lstm_model,data_test)
-
-y_0 = y[:,0]
-y_1 = y[:,1]
-y_2 = y[:,2]
-y_hat_0 = y_hat[:,0]
-y_hat_1 = y_hat[:,1]
-y_hat_2 = y_hat[:,2]
-
-x_values = range(len(y_0))
-
-
-plt.figure()
-plt.plot(x_values, y_1, color='blue', label='test_data' , linestyle='-')
-plt.plot(x_values, y_hat_1, color='green', label='prediction' , linestyle='-')
-plt.legend()
-
-plt.figure()
-plt.plot(x_values, y_2, color='red', label='test_data' , linestyle='-')
-plt.plot(x_values, y_hat_2, color='green', label='prediction' , linestyle='-')
-plt.legend()
-
-plt.figure()
-plt.plot(x_values, y_0, color='orange', label='test_data' , linestyle='-')
-plt.plot(x_values, y_hat_0, color='green', label='prediction' , linestyle='-')
-plt.legend()
-plt.show()
-
-'''
